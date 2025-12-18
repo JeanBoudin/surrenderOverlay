be6a6aa556d94383ed25c5c829b0cb56830ce109
m.vaugoyeau <m.vaugoyeau@cicd.biz>
2025-12-18 14:32:22 +0100

    Initial Commit

---
+++ SurrenderOverlay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SurrenderOverlay
+//
+//  Created by Maxime VAUGOYEAU on 18/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SurrenderOverlay/SurrenderOverlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurrenderOverlayApp.swift
+//  SurrenderOverlay
+//
+//  Created by Maxime VAUGOYEAU on 18/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct SurrenderOverlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
